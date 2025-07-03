a183b6f8adb85b24b25f3bdb3dba0df091f9037b
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-03 22:37:09 +0530

    frontend corrected path

--- MyBag/UI/vite.config.js
+++ MyBag/UI/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     proxy: {
       '/api': {
         target: 'https://mybag-server-mern.onrender.com',
