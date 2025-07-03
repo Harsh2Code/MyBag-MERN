ad67ee37b3cf09c8567fef151e87e4a97ffac545
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-04 00:04:23 +0530

    preview added

--- MyBag/UI/vite.config.js
+++ MyBag/UI/vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     historyApiFallback: true,
     proxy: {
       '/api': {
@@ -16,4 +17,7 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    allowedHosts: ['mybag-ui-mern.onrender.com']
+  }
 })
